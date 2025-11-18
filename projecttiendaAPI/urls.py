05a7c3ac43e620f43a95c7d122187d91745fede2
Euclides Medina Dagua <euclidesmeda10@gmail.com>
2025-11-18 17:32:59 -0500

    fix: corregir ruta /

--- projecttiendaAPI/urls.py
+++ projecttiendaAPI/urls.py
@@ -21,5 +21,5 @@ from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', GraphQLView.as_view(graphiql=True, schema=schema))
+    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema))
 ]
